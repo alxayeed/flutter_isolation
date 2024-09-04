ee7f5610e4563be6c8cbe51ecb05e5c84b17bd75
alxayeed <alxayeed@gmail.com>
2024-09-04 16:40:00 +0600

    Add: compute and spawn added. UI updated.

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'dart:isolate';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,8 @@ class Home extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const SizedBox(height: 50),
+
               const SizedBox(
                 height: 200,
                 width: 200,
@@ -31,7 +34,7 @@ class Home extends StatelessWidget {
                   final executionTimeInSeconds =
                       stopwatch.elapsedMilliseconds / 1000;
                   var snackBar = SnackBar(
-                    backgroundColor: Colors.green,
+                    backgroundColor: Colors.blue,
                     content: Text(
                         "Result is: ${result.toString()}. ($executionTimeInSeconds s)"),
                   );
@@ -39,34 +42,123 @@ class Home extends StatelessWidget {
                   ScaffoldMessenger.of(context).showSnackBar(snackBar);
                 },
               ),
-              MaterialButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: const Text('Isolate'),
-                onPressed: () async {
-                  final stopwatch = Stopwatch()..start();
-
-                  double result = await Isolate.run(() => doHeavyComputation());
-
-                  stopwatch.stop();
-                  final executionTimeInSeconds =
-                      stopwatch.elapsedMilliseconds / 1000;
-
-                  var snackBar = SnackBar(
-                    backgroundColor: Colors.green,
-                    content: Text(
-                        "Result is: ${result.toString()}. ($executionTimeInSeconds s)"),
-                  );
-
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                },
+              Expanded(
+                child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  padding: const EdgeInsets.all(20.0),
+                  children: [
+                    MaterialButton(
+                      color: Colors.green,
+                      textColor: Colors.white,
+                      child: const Text('Compute'),
+                      onPressed: () async {
+                        final stopwatch = Stopwatch()..start();
+                        double result = await compute(computeHeavyTask, 0);
+                
+                        stopwatch.stop();
+                        final executionTimeInSeconds =
+                            stopwatch.elapsedMilliseconds / 1000;
+                        var snackBar = SnackBar(
+                          backgroundColor: Colors.green,
+                          content: Text(
+                              "Result is: ${result.toString()}. ($executionTimeInSeconds s)"),
+                        );
+                
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      },
+                    ),
+                    MaterialButton(
+                      color: Colors.deepPurple.shade500,
+                      textColor: Colors.white,
+                      child: const Text('Isolate.run()'),
+                      onPressed: () async {
+                        final stopwatch = Stopwatch()..start();
+                
+                        double result = await Isolate.run(() => doHeavyComputation());
+                
+                        stopwatch.stop();
+                        final executionTimeInSeconds =
+                            stopwatch.elapsedMilliseconds / 1000;
+                
+                        var snackBar = SnackBar(
+                          backgroundColor: Colors.deepPurple.shade500,
+                          content: Text(
+                              "Result is: ${result.toString()}. ($executionTimeInSeconds s)"),
+                        );
+                
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      },
+                    ),
+                    MaterialButton(
+                      color: Colors.deepOrangeAccent,
+                      textColor: Colors.white,
+                      child: const Text('Isolate.spawn()'),
+                      onPressed: () async {
+                        final stopwatch = Stopwatch()..start();
+                
+                        final receivePort = ReceivePort();
+                
+                        try {
+                          await Isolate.spawn(
+                              doAnotherHeavyComputation, receivePort.sendPort);
+                
+                          // Using first to auto-close the ReceivePort after receiving the first message
+                          final message = await receivePort.first;
+                
+                          stopwatch.stop();
+                          final executionTimeInSeconds =
+                              stopwatch.elapsedMilliseconds / 1000;
+                
+                          var snackBar = SnackBar(
+                            backgroundColor: Colors.deepOrangeAccent,
+                            content: Text(
+                              "Result is: ${message.toString()}. ($executionTimeInSeconds s)",
+                            ),
+                          );
+                
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        } catch (error) {
+                          var snackBar = SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text("An error occurred: $error"),
+                          );
+                
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        } finally {
+                          receivePort.close();
+                        }
+                      },
+                    ),
+                    // MaterialButton(
+                    //   color: Colors.grey,
+                    //   textColor: Colors.white,
+                    //   child: const Text('Work Manager'),
+                    //   onPressed: () async {
+                    //   },
+                    // ),
+                  ],
+                ),
               ),
+
             ],
           ),
         ),
       ),
     );
   }
+}
+
+double computeHeavyTask(int startingPoint) {
+  double result = 0.0;
+
+  for (int i = startingPoint; i < 1000000000; i++) {
+    result += i;
+  }
+  return result;
 }
 
 Future<double> doHeavyComputation() async {
@@ -76,4 +168,14 @@ Future<double> doHeavyComputation() async {
     result += i;
   }
   return result;
+}
+
+void doAnotherHeavyComputation(SendPort sendPort) async {
+  double result = 0.0;
+
+  for (int i = 0; i < 1000000000; i++) {
+    result += i;
+  }
+  Isolate.exit(sendPort, result);
+  // sendPort.send(result);
 }
